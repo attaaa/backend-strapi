45045d7c34088dabfc013a42007e85efeace9e25
Attaaa <hatta.eka.p@gmail.com>
2022-05-17 21:39:16 +0800

    update and configure new scl backend

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 module.exports = {
   /**
@@ -7,7 +7,33 @@ module.exports = {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/*{ strapi }*/) {},
+  register({ strapi }) {
+    const extensionService = strapi.plugin("graphql").service("extension");
+    extensionService.use(({ nexus }) => ({
+      types: [
+        nexus.extendType({
+          type: "UsersPermissionsMe",
+          definition(t) {
+            // here define fields you need
+            t.int("role_id");
+          },
+        }),
+      ],
+      resolversConfig: {
+        // 'Mutation.deleteUsersPermissionsUser': {
+        //   auth: {
+        //     scope: ['plugin::users-permissions.user.find']
+        //   }
+        // },
+        // 'Mutation.deleteUsersPermissionsRole': {
+        //   auth: false
+        // },
+        // 'Query.usersPermissionsRole': {
+        //   auth: false
+        // }
+      },
+    }));
+  },
 
   /**
    * An asynchronous bootstrap function that runs before
@@ -16,5 +42,13 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  bootstrap({ strapi }) {},
+
+  /**
+   * An asynchronous destroy function that runs before
+   * your application gets shut down.
+   *
+   * This gives you an opportunity to gracefully stop services you run.
+   */
+  destroy({ strapi }) {},
 };
